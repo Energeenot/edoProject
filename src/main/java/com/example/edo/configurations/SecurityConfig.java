5ad286311c983e9db487ca2940636ce93426b6b5
Energeenot <johndoe@abramov@pandos@yandex.ru>
2023-12-07 18:40:20 +0300

    убрал одну страницу из доступа

--- src/main/java/com/example/edo/configurations/SecurityConfig.java
+++ src/main/java/com/example/edo/configurations/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/**", "/home", "/validator", "/registration", "/login").permitAll()
+                        .requestMatchers("/**", "/registration", "/login").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form
