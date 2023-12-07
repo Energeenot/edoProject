d620f1d986a2b99dd0e2db656c212bc15d7edf79
Energeenot <johndoe@abramov@pandos@yandex.ru>
2023-12-07 18:38:54 +0300

    добавил метод записи доп инфы в таблицу

--- src/main/java/com/example/edo/controllers/personalAccountController.java
+++ src/main/java/com/example/edo/controllers/personalAccountController.java
@@ -1,18 +1,23 @@
 package com.example.edo.controllers;
 
 import com.example.edo.Models.User;
+import com.example.edo.repositories.UserRepository;
 import com.example.edo.services.UserService;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class personalAccountController {
     private final UserService userService;
+    private final UserRepository userRepository;
 
     @GetMapping("/personalAccount")
     public String personalAccount(Principal principal, Model model){
@@ -20,4 +25,22 @@ public class personalAccountController {
         model.addAttribute("user", user);
         return "personalAccount";
     }
+
+    @PostMapping("/personalAccount")
+    public String supplementInformation(
+            @RequestParam String name,
+            @RequestParam String mail,
+            @RequestParam String numberGroup,
+            @RequestParam String teacherMail,
+            Model model){
+        User user = userRepository.findByMail(mail);
+        model.addAttribute("user", user);
+        user.setName(name);
+        user.setMail(mail);
+        user.setNumberGroup(numberGroup);
+        user.setTeacherMail(teacherMail);
+        userRepository.saveAndFlush(user);
+        return "personalAccount";
+    }
 }
+//http://localhost:8080/personalAccount?qwe=имя
