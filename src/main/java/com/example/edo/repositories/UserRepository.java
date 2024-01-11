7735cfb68073eb024c195cdb7f6e8553a49f7b74
Energeenot <johndoe@abramov@pandos@yandex.ru>
2024-01-12 00:58:05 +0300

    переименовал папку

--- src/main/java/com/example/edo/repositories/UserRepository.java
+++ src/main/java/com/example/edo/repositories/UserRepository.java
@@ -1,6 +1,6 @@
 package com.example.edo.repositories;
 
-import com.example.edo.Models.User;
+import com.example.edo.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
