ca56c78c36ad26576ead68d39a938cd3da472f58
Energeenot <johndoe@abramov@pandos@yandex.ru>
2024-01-12 00:53:57 +0300

    переименовал папку

--- src/main/java/com/example/edo/repositories/PostRepository.java
+++ src/main/java/com/example/edo/repositories/PostRepository.java
@@ -1,6 +1,6 @@
 package com.example.edo.repositories;
 
-import com.example.edo.Models.Post;
+import com.example.edo.models.Post;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PostRepository extends CrudRepository<Post, Long> {
