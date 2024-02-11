7aa67d943c475848429600d8512c495027f946b7
Energeenot <johndoe@abramov@pandos@yandex.ru>
2024-02-11 21:14:34 +0300

    написал сервис для работы с файлами

---
+++ src/main/java/com/example/edo/services/FilesService.java
@@ -0,0 +1,30 @@
+package com.example.edo.services;
+
+import com.example.edo.models.Files;
+import com.example.edo.repositories.FilesRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class FilesService {
+    private final FilesRepository filesRepository;
+
+    @Value("${file.storage.path}")
+    private String fileStoragePath;
+
+    public List<String> findAllUniqueNamesByUniqueGroupCode(String uniqueGroupCode){
+        return filesRepository.findAllUniqueNamesByUniqueGroupCode(uniqueGroupCode);
+    }
+
+    public Path getFilePathByName(String fileName) {
+        return Paths.get(fileStoragePath, fileName);
+    }
+}
