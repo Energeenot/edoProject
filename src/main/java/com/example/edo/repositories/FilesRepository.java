5aac32d6368d28c7f527afb1f3d100e91b48d67f
Energeenot <johndoe@abramov@pandos@yandex.ru>
2024-02-11 21:11:38 +0300

    добавил запрос поиска файла с уникальным групповым кодом

--- src/main/java/com/example/edo/repositories/FilesRepository.java
+++ src/main/java/com/example/edo/repositories/FilesRepository.java
@@ -2,6 +2,12 @@ package com.example.edo.repositories;
 
 import com.example.edo.models.Files;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface FilesRepository extends JpaRepository<Files, Long> {
+    @Query("SELECT f.uniqueName FROM Files f WHERE f.uniqueGroupCode = :uniqueGroupCode")
+    List<String> findAllUniqueNamesByUniqueGroupCode(@Param("uniqueGroupCode") String uniqueGroupCode);
 }
