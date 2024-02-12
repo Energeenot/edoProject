fa23d4c6fd085d56e761ae540c2b37e25f8f2086
Energeenot <johndoe@abramov@pandos@yandex.ru>
2024-02-12 19:38:12 +0300

    убран таймлиф

--- src/main/java/com/example/edo/configurations/MvcConfig.java
+++ src/main/java/com/example/edo/configurations/MvcConfig.java
@@ -5,8 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
-import org.thymeleaf.templatemode.TemplateMode;
+//import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+//import org.thymeleaf.templatemode.TemplateMode;
 
 @Configuration
 @EnableWebMvc
