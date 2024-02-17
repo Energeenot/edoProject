e9014314290b67c45d4427584474b5f980433990
Energeenot <johndoe@abramov@pandos@yandex.ru>
2024-02-17 13:55:44 +0300

    добавил валидацию полей при входе

--- src/main/java/com/example/edo/controllers/LoginController.java
+++ src/main/java/com/example/edo/controllers/LoginController.java
@@ -1,13 +1,20 @@
 package com.example.edo.controllers;
 
+import com.example.edo.models.User;
 import com.example.edo.services.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.security.Principal;
+import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 @Controller
 @RequiredArgsConstructor
@@ -27,14 +34,25 @@ public class LoginController {
 //    }
 
     @GetMapping("/login")
-    public String login(Model model, Principal principal, @RequestParam(name = "error", required = false) String error, @RequestParam(name = "logout", required = false) String logout) {
-        model.addAttribute("user", userService.getUserByPrincipal(principal));
-        if (error != null) {
-            model.addAttribute("errorMessage", "Неверный логин или пароль");
+    public String login(@Valid User user, BindingResult bindingResult, Model model, Principal principal, @RequestParam(name = "error", required = false) String error, @RequestParam(name = "logout", required = false) String logout) {
+        model.addAttribute("user", user);
+        if (bindingResult.hasErrors()) {
+            Collector<FieldError, ?, Map<String, String>> collector = Collectors.toMap(
+                    fieldError -> fieldError.getField() + "Error",
+                    FieldError::getDefaultMessage
+            );
+            Map<String, String> errorsMap = bindingResult.getFieldErrors().stream().collect(collector);
+            model.mergeAttributes(errorsMap);
+            return "login";
+        } else {
+            model.addAttribute("user", userService.getUserByPrincipal(principal));
+            if (error != null) {
+                model.addAttribute("errorMessage", "Неверный логин или пароль");
+            } else if (logout != null) {
+                model.addAttribute("errorMessage", "Успешный выход");
+            }
+            return "login";
         }
-        else if(logout != null){
-            model.addAttribute("errorMessage", "Успешный выход");
-        }
-        return "login";
     }
+
 }
