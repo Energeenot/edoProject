8e299ede9ef3b7fad25ed33a4f8c0cb569becc01
Energeenot <johndoe@abramov@pandos@yandex.ru>
2024-02-17 13:57:18 +0300

    добавил валидацию полей при регистрации

--- src/main/java/com/example/edo/controllers/RegistrationController.java
+++ src/main/java/com/example/edo/controllers/RegistrationController.java
@@ -4,14 +4,21 @@ import com.example.edo.models.User;
 import com.example.edo.repositories.UserRepository;
 import com.example.edo.services.UserService;
 import jakarta.servlet.http.HttpSession;
+
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.security.Principal;
+import java.util.stream.Collectors;
+import java.util.Map;
+import java.util.stream.Collector;
 
 @Controller
 @RequiredArgsConstructor
@@ -29,16 +36,32 @@ public class RegistrationController {
     }
 
     @PostMapping("/registration")
-    public String createUser(User user, Model model, HttpSession session){
+    public String createUser(@Valid User user, BindingResult bindingResult, Model model, HttpSession session){
         model.addAttribute("user", user);
-        String message = userService.createUser(user);
-        if (message.equals("true")){
-            session.setAttribute("successMessage", "Успешная регистрация");
-            return "redirect:/login";
-        }else {
-            model.addAttribute("errorMessage", "Аккаунт с такой почтой уже есть");
+        if (bindingResult.hasErrors()){
+            Collector<FieldError, ?, Map<String, String>> collector = Collectors.toMap(
+                    fieldError -> fieldError.getField() + "Error",
+                    FieldError::getDefaultMessage
+            );
+            Map<String, String> errorsMap = bindingResult.getFieldErrors().stream().collect(collector);
+            model.mergeAttributes(errorsMap);
             return "registration";
+//            bindingResult.getFieldErrors().stream().collect(Collectors.toMap(
+//                    fieldError -> fieldError.getField() + "Error",
+//                    FieldError::getDefaultMessage
+//            ))
+//            model.addAttribute("bindingResult", bindingResult);
+//            return "registration";
+        } else {
+            String message = userService.createUser(user);
+            if (message.equals("true")){
+                session.setAttribute("successMessage", "Успешная регистрация");
+                return "redirect:/login";
+            }else {
+                model.addAttribute("errorMessage", "Аккаунт с такой почтой уже есть");
+                model.addAttribute("bindingResult", bindingResult);
+                return "registration";
+            }
         }
-
     }
 }
