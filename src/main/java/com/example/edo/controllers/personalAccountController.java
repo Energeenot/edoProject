4018dc60aca217113c4f5746b55099ac8384c22e
Energeenot <johndoe@abramov@pandos@yandex.ru>
2024-04-01 18:12:01 +0300

    добавил метод создания задач

--- src/main/java/com/example/edo/controllers/personalAccountController.java
+++ src/main/java/com/example/edo/controllers/personalAccountController.java
@@ -1,8 +1,12 @@
 package com.example.edo.controllers;
 
+import com.example.edo.models.Task;
 import com.example.edo.models.User;
+import com.example.edo.repositories.TaskRepository;
 import com.example.edo.repositories.UserRepository;
 import com.example.edo.services.UserService;
+import jakarta.mail.Session;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -10,6 +14,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -17,14 +23,48 @@ import java.security.Principal;
 public class personalAccountController {
     private final UserService userService;
     private final UserRepository userRepository;
+    private final TaskRepository taskRepository;
 
     @GetMapping("/personalAccount")
     public String personalAccount(Principal principal, Model model){
         model.addAttribute("user", userService.getUserByPrincipal(principal));
+        if (userService.getUserByPrincipal(principal).isTeacher()){
+            List<String> numberGroups = userRepository.findAllNumberGroup();
+            if (!numberGroups.isEmpty()){
+                System.out.println("-----------List: " + numberGroups);
+                model.addAttribute("numberGroups", numberGroups);
+            }
+            List<Task> allTasks = taskRepository.findAll();
+//            List<String> numberGroupsList = new ArrayList<>();
+            if (!allTasks.isEmpty()){
+                model.addAttribute("tasks", allTasks);
+//                for (Task task: allTasks){
+////                     numberGroupsList.add(task.getUser().getNumberGroup());
+//                    System.out.println(task);
+//                    model.addAttribute("taskMessage", task.getMessage());
+//                    model.addAttribute("studentName", task.getUser().getName());
+//                    model.addAttribute("taskStage", task.getStage());
+//                    if (task.getFiles() == null){
+//                        model.addAttribute("taskUniqueGroupCode", "Не отправлен");
+//                    }else {
+//                        model.addAttribute("taskUniqueGroupCode", task.getFiles().getUniqueGroupCode());
+//                    }
+//                    model.addAttribute("taskDeadline", task.getDeadline());
+//                }
+//                System.out.println(numberGroupsList);
+//                model.addAttribute("allTasks", allTasks);
+                System.out.println(allTasks);
+            }
+
+//            System.out.println(model.getAttribute("numberGroups"));
+        }else{
+            model.addAttribute("tasks", taskRepository.findAllByUserId(userService.getUserByPrincipal(principal).getId()));
+            System.out.println(taskRepository.findAllByUserId(userService.getUserByPrincipal(principal).getId()));
+        }
         return "personalAccount";
     }
 
-    @PostMapping("/personalAccount")
+    @PostMapping("/personalAccount/supplementInformation")
     public String supplementInformation(
             @RequestParam String name,
             @RequestParam String mail,
@@ -41,6 +81,33 @@ public class personalAccountController {
         user.setMail(mail);
         user.setNumberGroup(numberGroup);
         userRepository.saveAndFlush(user);
+        model.addAttribute("message", "Данные успешно обновлены");
+        return "personalAccount";
+    }
+
+    @PostMapping("/personalAccount/createTask")
+    public String createTask(@RequestParam List<String> selectedGroups, @RequestParam String message, @RequestParam String deadline, Model model, Principal principal){
+        model.addAttribute("user", userService.getUserByPrincipal(principal));
+        List<String> numberGroups = userRepository.findAllNumberGroup();
+        model.addAttribute("numberGroups", numberGroups);
+        System.out.println(selectedGroups);
+        // создание таска скорее всего надо перенести в тасксервис
+        for (String numberGroupElement: selectedGroups){
+            List<User> usersList= userRepository.findAllByNumberGroup(numberGroupElement);
+            for (User certainUser: usersList){
+                Task task = new Task();
+                task.setMessage(message);
+                task.setSender(userService.getUserByPrincipal(principal));
+                task.setUser(certainUser);
+                task.setDeadline(deadline);
+                task.setStage("Не выполнено");
+                System.out.println(task);
+                taskRepository.save(task);
+                model.addAttribute("tasks", taskRepository.findAll());
+//                System.out.println(taskRepository.findAllTasksWithReplacedNull());
+            }
+        }
+
         return "personalAccount";
     }
 }
