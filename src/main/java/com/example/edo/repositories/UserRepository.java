716f6b1a84cc32ac13e63e43eb6d1eababe53f3a
Energeenot <johndoe@abramov@pandos@yandex.ru>
2024-04-01 18:17:03 +0300

    добавил методы для поиска пользователей по номеру группы, и поиск номеров групп

--- src/main/java/com/example/edo/repositories/UserRepository.java
+++ src/main/java/com/example/edo/repositories/UserRepository.java
@@ -2,7 +2,20 @@ package com.example.edo.repositories;
 
 import com.example.edo.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByMail(String mail);
+    User findByNumberGroup(String numberGroup);
+
+    @Query("SELECT u FROM User u WHERE u.numberGroup = :numberGroup")
+    List<User> findAllByNumberGroup(@Param("numberGroup") String numberGroup);
+
+//    @Query("SELECT DISTINCT u.numberGroup FROM User u")
+    @Query("SELECT DISTINCT u.numberGroup FROM User u WHERE u.numberGroup IS NOT NULL")
+    List<String> findAllNumberGroup();
+
 }
