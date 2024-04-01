21846870ad10f7249016e0f7d179bab0714b5e50
Energeenot <johndoe@abramov@pandos@yandex.ru>
2024-04-01 18:13:23 +0300

    удалил бесполезные классы

--- src/main/java/com/example/edo/repositories/PostRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.edo.repositories;
-
-import com.example.edo.models.Post;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PostRepository extends CrudRepository<Post, Long> {
-
-}
