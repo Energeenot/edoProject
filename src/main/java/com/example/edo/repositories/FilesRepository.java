c9803fffdb76623bdf36f4b9a7fc7c43de287258
Energeenot <johndoe@abramov@pandos@yandex.ru>
2024-04-05 01:04:43 +0300

    добавил поиск имени файла по групповому полю

--- src/main/java/com/example/edo/repositories/FilesRepository.java
+++ src/main/java/com/example/edo/repositories/FilesRepository.java
@@ -10,4 +10,9 @@ import java.util.List;
 public interface FilesRepository extends JpaRepository<Files, Long> {
     @Query("SELECT f.uniqueName FROM Files f WHERE f.uniqueGroupCode = :uniqueGroupCode")
     List<String> findAllUniqueNamesByUniqueGroupCode(@Param("uniqueGroupCode") String uniqueGroupCode);
+
+    @Query("SELECT f.uniqueName FROM Files f WHERE f.uniqueGroupCode = :uniqueGroupCode")
+    String findUniqueNameByUniqueGroupCode(@Param("uniqueGroupCode") String uniqueGroupCode);
+
+
 }
