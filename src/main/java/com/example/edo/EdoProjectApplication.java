78d42cbb656b1ae1f2855b423cc597402dadb752
Energeenot <abramov.pandos@yandex.ru>
2024-10-14 19:56:14 +0300

    убрал тудушки и неиспользуемые импорты

--- src/main/java/com/example/edo/EdoProjectApplication.java
+++ src/main/java/com/example/edo/EdoProjectApplication.java
@@ -1,20 +1,12 @@
 package com.example.edo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-//@AutoConfiguration
-
 public class EdoProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EdoProjectApplication.class, args);
 	}
-	//TODO: разобраться с css validator
-	//TODO: нужна ли мне бд? стоит попробовать использовать облако, потому что при загрузке данных в бд, записывается только путь к файлам, а если с другого пк попробуют запустить проект, то файлы не загрузятся
-	//todo: добавить проверки на пустую отправку и тд, в целом в проекте
 }
