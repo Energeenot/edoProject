16d8b76a7d94515d715d4a9d60ca1a7b857499cb
Energeenot <abramov.pandos@yandex.ru>
2024-10-14 19:41:20 +0300

    убрал лишние комментарии

--- src/main/java/com/example/edo/controllers/HelloController.java
+++ src/main/java/com/example/edo/controllers/HelloController.java
@@ -11,12 +11,11 @@ import java.security.Principal;
 @Controller
 @RequiredArgsConstructor
 public class HelloController {
+
     private final UserService userService;
+
     @GetMapping("/")
-//    @RequestMapping("/") //при переходе на страницу выполняется метод
-    //@RequestParam(value="name", required=false, defaultValue="World") String name,
     public String home(Model model, Principal principal) {
-//        model.addAttribute("title", "Главная страница");
         model.addAttribute("user", userService.getUserByPrincipal(principal));
         return "home";
     }
