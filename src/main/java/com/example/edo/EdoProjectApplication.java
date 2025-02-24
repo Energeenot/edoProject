246297faabf8f05a047d46a86098f82336429bcd
Energeenot <abramov.pandos@yandex.ru>
2025-02-24 16:57:04 +0300

    добавлена аннотация для работы с feign

--- src/main/java/com/example/edo/EdoProjectApplication.java
+++ src/main/java/com/example/edo/EdoProjectApplication.java
@@ -2,10 +2,12 @@ package com.example.edo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.kafka.annotation.EnableKafka;
 
 @EnableKafka
 @SpringBootApplication
+@EnableFeignClients(basePackages = "com.example.edo.feign")
 public class EdoProjectApplication {
 
 	public static void main(String[] args) {
