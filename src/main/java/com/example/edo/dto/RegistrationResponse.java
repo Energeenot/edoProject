3ea2d561ab6bf05084b23bfeeb9c28f42edc4ebb
Energeenot <abramov.pandos@yandex.ru>
2025-03-04 15:17:27 +0300

    написал дто для ответа от сервиса аутентификации

---
+++ src/main/java/com/example/edo/dto/RegistrationResponse.java
@@ -0,0 +1,11 @@
+package com.example.edo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class RegistrationResponse {
+    private String uuid;
+    private String role;
+}
