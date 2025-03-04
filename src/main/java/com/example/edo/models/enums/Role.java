009974013ed34ae975f97f31a18ecf542cf4b1ff
Energeenot <abramov.pandos@yandex.ru>
2025-03-04 15:18:19 +0300

    переписал роли теперь они капсом

--- src/main/java/com/example/edo/models/enums/Role.java
+++ src/main/java/com/example/edo/models/enums/Role.java
@@ -3,7 +3,7 @@ package com.example.edo.models.enums;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Role implements GrantedAuthority {
-    ROLE_student, ROLE_teacher;
+    ROLE_STUDENT, ROLE_TEACHER;
 
     @Override
     public String getAuthority() {
