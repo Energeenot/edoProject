a70a75da13e6ca569b7f91a6437bec04655da8fd
Energeenot <abramov.pandos@yandex.ru>
2025-03-04 16:34:19 +0300

    переписал методы для регистрации

--- src/main/java/com/example/edo/controllers/RegistrationController.java
+++ src/main/java/com/example/edo/controllers/RegistrationController.java
@@ -1,56 +1,42 @@
 package com.example.edo.controllers;
 
+import com.example.edo.dto.RegistrationRequest;
 import com.example.edo.models.User;
 import com.example.edo.services.UserService;
-import jakarta.servlet.http.HttpSession;
+import com.example.edo.util.ValidationErrorMapper;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import java.security.Principal;
-import java.util.Map;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
 
 @Controller
 @RequiredArgsConstructor
 public class RegistrationController {
+
     private final UserService userService;
 
     @GetMapping("/registration")
-    public String registration(Model model, Principal principal){
+    public String registration(@ModelAttribute("user") User user, Model model){
         model.addAttribute("title", "Страница регистрации");
-        model.addAttribute("user", userService.getUserByPrincipal(principal));
         return "registration";
-
     }
 
     @PostMapping("/registration")
-    public String createUser(@Valid User user, BindingResult bindingResult, Model model, HttpSession session){
-        model.addAttribute("user", user);
-        if (bindingResult.hasErrors()){
-            Collector<FieldError, ?, Map<String, String>> collector = Collectors.toMap(
-                    fieldError -> fieldError.getField() + "Error",
-                    FieldError::getDefaultMessage
-            );
-            Map<String, String> errorsMap = bindingResult.getFieldErrors().stream().collect(collector);
-            model.mergeAttributes(errorsMap);
+    public String registration(@Valid @ModelAttribute RegistrationRequest registrationRequest,
+                               BindingResult bindingResult, Model model, HttpServletRequest request){
+        model.addAttribute("user", userService.getUserByRequest(request));
+        if (bindingResult.hasErrors()) {
+            model.addAllAttributes(ValidationErrorMapper.mapErrors(bindingResult));
             return "registration";
-        } else {
-            String message = userService.createUser(user);
-            if (message.equals("true")){
-                session.setAttribute("successMessage", "Успешная регистрация");
-                return "redirect:/login";
-            }else {
-                model.addAttribute("errorMessage", "Аккаунт с такой почтой уже есть");
-                model.addAttribute("bindingResult", bindingResult);
-                return "registration";
-            }
         }
+        userService.createUser(registrationRequest);
+        return "redirect:/login";
+
+        //todo: обернуть в сагу
     }
 }
