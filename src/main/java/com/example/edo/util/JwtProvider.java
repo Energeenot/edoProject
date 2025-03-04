b4d05d4b1063027ec167f35e21ca16e0282f31ec
Energeenot <abramov.pandos@yandex.ru>
2025-03-04 15:11:07 +0300

    добавил методы для получения uuid и role из jwt,  проверка подписи в нём

--- src/main/java/com/example/edo/util/JwtProvider.java
+++ src/main/java/com/example/edo/util/JwtProvider.java
@@ -19,24 +19,51 @@ public class JwtProvider {
     public String getEmailFromToken(String token) {
         try {
             return JWT.require(Algorithm.HMAC256(secret))
+                    .withIssuer("auth-service")
                     .build()
                     .verify(token)
                     .getSubject();
         } catch (JWTVerificationException e) {
-            log.error("Invalid JWT  {}", e.getMessage());
-            throw new RuntimeException("Invalid JWT");
+            log.error("Error while take email  {}", e.getMessage());
+            throw new RuntimeException("Invalid JWT in method getEmailFromToken");
+        }
+    }
+
+    public String getRoleFromToken(String token) {
+        try {
+            return JWT.require(Algorithm.HMAC256(secret))
+                    .withIssuer("auth-service")
+                    .build()
+                    .verify(token)
+                    .getClaim("role").asString();
+        } catch (JWTVerificationException e) {
+            log.error("Error while take role {}", e.getMessage());
+            throw new RuntimeException("Invalid JWT in method getRoleFromToken");
+        }
+    }
+
+    public String getUuidFromToken(String token) {
+        try{
+            return JWT.require(Algorithm.HMAC256(secret))
+                    .withIssuer("auth-service")
+                    .build()
+                    .verify(token)
+                    .getClaim("uuid").asString();
+        } catch (JWTVerificationException e) {
+            throw new RuntimeException("Invalid JWT in method getUuidFromToken " + e);
         }
     }
 
     public boolean isTokenExpired(String token) {
         try {
             Date expiration = JWT.require(Algorithm.HMAC256(secret))
+                    .withIssuer("auth-service")
                     .build()
                     .verify(token)
                     .getExpiresAt();
             return expiration.before(new Date());
         } catch (JWTVerificationException e) {
-            log.warn("Failed to parse JWT {}", e.getMessage());
+            log.warn("Failed to parse JWT {}", e.getMessage(), e);
             return true;
         }
     }
@@ -44,6 +71,7 @@ public class JwtProvider {
     public boolean validateToken(String token) {
         try {
             JWT.require(Algorithm.HMAC256(secret))
+                    .withIssuer("auth-service")
                     .build()
                     .verify(token);
             return true;
