525298b4d51039d6bdf8d62eec4089b446680b3b
Energeenot <abramov.pandos@yandex.ru>
2025-03-04 15:19:31 +0300

    переписал секурити конфиг под jwt

--- src/main/java/com/example/edo/configurations/SecurityConfig.java
+++ src/main/java/com/example/edo/configurations/SecurityConfig.java
@@ -1,14 +1,18 @@
 package com.example.edo.configurations;
 
+import com.example.edo.util.JwtFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
 
 @EnableMethodSecurity
@@ -16,24 +20,30 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final JwtFilter jwtFilter;
+    private final RequestMatcher publicPaths;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/static/**", "/registration", "/login", "/forgotPassword", "/checkCode", "/resetPassword").permitAll()
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(publicPaths).permitAll()
                         .anyRequest().authenticated()
                 )
-                .formLogin((form) -> form
+                .formLogin(form -> form
                         .loginPage("/login")
-                        .defaultSuccessUrl("/personalAccount")
                         .permitAll()
+                        .disable()
                 )
                 .logout(logout -> logout
                         .logoutUrl("/logout")
-                        .logoutSuccessUrl("/login") // Указываем URL для перенаправления после выхода из учетной записи
+                        .logoutSuccessUrl("/login")
+                        .deleteCookies("JWT", "REFRESH_TOKEN")
                         .permitAll()
-                );
-//                .logout(LogoutConfigurer::permitAll);
+                )
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .anonymous(AbstractHttpConfigurer::disable);
         return http.build();
     }
 
