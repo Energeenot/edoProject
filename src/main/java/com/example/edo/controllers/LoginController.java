71c03234806e2de916984294c179e7f07ad2d3cb
Energeenot <abramov.pandos@yandex.ru>
2025-03-27 21:00:42 +0300

    переписал методы под работу с jwt

--- src/main/java/com/example/edo/controllers/LoginController.java
+++ src/main/java/com/example/edo/controllers/LoginController.java
@@ -1,49 +1,59 @@
 package com.example.edo.controllers;
 
+import com.example.edo.dto.TokenResponse;
 import com.example.edo.models.User;
-import com.example.edo.services.UserService;
-import jakarta.validation.Valid;
+import com.example.edo.services.AuthClientService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.security.Principal;
-import java.util.Map;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
 
 @Controller
 @RequiredArgsConstructor
 public class LoginController {
 
-    private final UserService userService;
+    private final AuthClientService authClientService;
 
-    @GetMapping("/login")
-    public String login(@Valid User user, BindingResult bindingResult, Model model, Principal principal,
-                        @RequestParam(name = "error", required = false) String error, @RequestParam(name = "logout",
-            required = false) String logout) {
-        model.addAttribute("user", user);
-        if (bindingResult.hasErrors()) {
-            Collector<FieldError, ?, Map<String, String>> collector = Collectors.toMap(
-                    fieldError -> fieldError.getField() + "Error",
-                    FieldError::getDefaultMessage
-            );
-            Map<String, String> errorsMap = bindingResult.getFieldErrors().stream().collect(collector);
-            model.mergeAttributes(errorsMap);
-            return "login";
-        } else {
-            model.addAttribute("user", userService.getUserByPrincipal(principal));
-            if (error != null) {
-                model.addAttribute("errorMessage", "Неверный логин или пароль");
-            } else if (logout != null) {
-                model.addAttribute("errorMessage", "Успешный выход");
-            }
+    @PostMapping("/login")
+    public String login(@RequestParam String username, @RequestParam String password,
+                        HttpServletResponse response, HttpSession session,
+                        @ModelAttribute("user") User user, Model model) {
+        try {
+            TokenResponse tokenResponse = authClientService.authenticate(username, password);
+            System.out.println(tokenResponse);
+
+            Cookie cookie = new Cookie("JWT", tokenResponse.getAccessToken());
+            cookie.setHttpOnly(true);
+            cookie.setPath("/");
+            response.addCookie(cookie);
+
+            session.setAttribute("refreshToken", tokenResponse.getRefreshToken());
+
+            return "redirect:/personalAccount";
+        } catch (Exception e) {
+            model.addAttribute("error", e.getMessage());
             return "login";
         }
+    }
+
+    @GetMapping("/login")
+    public String login(@ModelAttribute("user") User user,
+                        @RequestParam(name = "error", required = false) String error,
+                        @RequestParam(name = "logout", required = false) String logout,
+                        Model model) {
+        if (error != null) {
+            model.addAttribute("errorMessage", "Неверный логин или пароль");
+        } else if (logout != null) {
+            model.addAttribute("errorMessage", "Успешный выход");
+        }
+
+        return "login";
     }
 
 }
