07a6e0131f6e08ea33efa145f3d7f671651a1af3
Energeenot <abramov.pandos@yandex.ru>
2025-03-27 21:32:57 +0300

    вынес бизнес логику в сервисы, переделал получение пользователя с principal на HttpServletRequest

--- src/main/java/com/example/edo/controllers/PersonalAccountController.java
+++ src/main/java/com/example/edo/controllers/PersonalAccountController.java
@@ -1,147 +1,96 @@
 package com.example.edo.controllers;
 
-import com.example.edo.dto.MessageDto;
-import com.example.edo.kafka.SenderProducer;
 import com.example.edo.models.Task;
 import com.example.edo.models.User;
-import com.example.edo.repositories.TaskRepository;
-import com.example.edo.repositories.UserRepository;
+import com.example.edo.services.NotificationService;
+import com.example.edo.services.TaskService;
 import com.example.edo.services.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.security.Principal;
 import java.util.List;
-import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
 @Slf4j
 public class PersonalAccountController {
     private final UserService userService;
-    private final UserRepository userRepository;
-    private final TaskRepository taskRepository;
-    private final SenderProducer producer;
+    private final TaskService taskService;
+    private final NotificationService notificationService;
 
     @GetMapping("/personalAccount")
-    public String personalAccount(Principal principal, Model model){
-        model.addAttribute("user", userService.getUserByPrincipal(principal));
-        if (userService.getUserByPrincipal(principal).isTeacher()){
-            List<String> numberGroups = userRepository.findAllNumberGroup();
+    public String personalAccount(HttpServletRequest request,
+                                  Model model, Principal principal){
+        log.info("GET /personalAccount вызван, principal = {}", principal.getName());
+        User user = userService.getUserByRequest(request);
+        model.addAttribute("user", user);
+        model.addAttribute("email", principal.getName());
+
+        if (user.isTeacher()){
+            List<String> numberGroups = userService.getNumberGroups();
             if (!numberGroups.isEmpty()){
-                System.out.println("-----------List: " + numberGroups);
                 model.addAttribute("numberGroups", numberGroups);
             }
-            List<Task> allTasks = taskRepository.findAll();
+            List<Task> allTasks = taskService.getAllTasks();
             if (!allTasks.isEmpty()){
                 model.addAttribute("tasks", allTasks);
                 System.out.println(allTasks);
             }
         }else{
-            model.addAttribute("tasks", taskRepository.findAllByUserId(userService
-                    .getUserByPrincipal(principal).getId()));
-            System.out.println(taskRepository.findAllByUserId(userService.getUserByPrincipal(principal).getId()));
+            model.addAttribute("tasks", taskService.getAllTasksByUserId(user.getId()));
         }
-        return "personalAccount";
-    }
-
-    @PostMapping("/personalAccount/supplementInformation")
-    public String supplementInformation(
-            @RequestParam String name,
-            @RequestParam String mail,
-            @RequestParam String numberGroup,
-            Model model, Principal principal){
-        model.addAttribute("user", userService.getUserByPrincipal(principal));
-        if (numberGroup.isEmpty()){
-            model.addAttribute("message", "Введите данные");
-            return "personalAccount";
-        }
-        User user = userRepository.findByMail(mail);
-        model.addAttribute("user", user);
-        user.setName(name);
-        user.setMail(mail);
-        user.setNumberGroup(numberGroup);
-        userRepository.saveAndFlush(user);
-        model.addAttribute("message", "Данные успешно обновлены");
         return "personalAccount";
     }
 
     @PostMapping("/personalAccount/createTask")
     public String createTask(@RequestParam List<String> selectedGroups, @RequestParam String message,
-                             @RequestParam String deadline, Model model, Principal principal){
-        model.addAttribute("user", userService.getUserByPrincipal(principal));
-        List<String> numberGroups = userRepository.findAllNumberGroup();
-        model.addAttribute("numberGroups", numberGroups);
-        System.out.println(selectedGroups);
-        // создание таска скорее всего надо перенести в тасксервис
-        for (String numberGroupElement: selectedGroups){
-            List<User> usersList= userRepository.findAllByNumberGroup(numberGroupElement);
-            for (User certainUser: usersList){
-                Task task = new Task();
-                task.setMessage(message);
-                task.setSender(userService.getUserByPrincipal(principal));
-                task.setUser(certainUser);
-                task.setDeadline(deadline);
-                task.setStage("Не выполнено");
-                System.out.println(task);
-                taskRepository.save(task);
-                model.addAttribute("tasks", taskRepository.findAll());
-            }
-        }
-
-        return "personalAccount";
+                             @RequestParam String deadline,
+                             Model model,  HttpServletRequest request, Principal principal){
+        log.info("Post /personalAccount/createTask вызван, principal = {}", principal.getName());
+        User user = userService.getUserByRequest(request);
+        model.addAttribute("user", user);
+        model.addAttribute("numberGroups", userService.getNumberGroups());
+        taskService.createTask(selectedGroups, message, deadline, user);
+        model.addAttribute("tasks", taskService.getAllTasks());
+        return "redirect:/personalAccount";
     }
 
     @PostMapping("/personalAccount/execute")
-    public String executeTask(@RequestParam String executeTaskId, Model model, Principal principal){
-        model.addAttribute("user", userService.getUserByPrincipal(principal));
-        Optional<Task> currentTask = taskRepository.findTaskById(Long.parseLong((String)executeTaskId));
-        Task desiredTask;
-        if (currentTask.isPresent()){
-            desiredTask = currentTask.get();
-            desiredTask.setStage("Задача выполнена");
-            taskRepository.save(desiredTask);
-        }
-
+    public String executeTask(@RequestParam String executeTaskId, Model model,
+                              Principal principal, HttpServletRequest request){
+        log.info("Post /personalAccount/execute вызван, principal = {}", principal.getName());
+        model.addAttribute("user", userService.getUserByRequest(request));
+        taskService.executeTask(executeTaskId);
         return "redirect:/personalAccount";
     }
 
     @PostMapping("/personalAccount/delete")
-    public String deleteTask(Model model, Principal principal, @RequestParam String deleteTaskId){
-        model.addAttribute("user", userService.getUserByPrincipal(principal));
-        Optional<Task> currentTask = taskRepository.findTaskById(Long.parseLong((String)deleteTaskId));
-        Task desiredTask;
-        if (currentTask.isPresent()){
-            desiredTask = currentTask.get();
-            taskRepository.deleteById(desiredTask.getId());
-        }
+    public String deleteTask(Model model, Principal principal,
+                             HttpServletRequest request, @RequestParam String deleteTaskId){
+        log.info("Post /personalAccount/delete вызван, principal = {}", principal.getName());
+        model.addAttribute("user", userService.getUserByRequest(request));
+        taskService.deleteTask(deleteTaskId);
         return "redirect:/personalAccount";
     }
 
     @PostMapping("/personalAccount/feedback")
     public String feedbackToStudent(Model model, Principal principal, @RequestParam String feedbackTaskId,
-                                    @RequestParam String feedbackInput){
-        model.addAttribute("user", userService.getUserByPrincipal(principal));
-        Optional<Task> currentTask = taskRepository.findTaskById(Long.parseLong((String) feedbackTaskId));
-        Task desiredTask;
-        if (currentTask.isPresent()){
-            desiredTask = currentTask.get();
-            desiredTask.setStage("Отправлены правки");
-            taskRepository.save(desiredTask);
-            String studentMail = desiredTask.getUser().getMail();
-            String teacherName = desiredTask.getSender().getName();
+                                    @RequestParam String feedbackInput, HttpServletRequest request){
+        log.info("Post /personalAccount/feedback вызван, principal = {}", principal.getName());
+        model.addAttribute("user", userService.getUserByRequest(request));
 
-            producer.sendFeedback(MessageDto.builder()
-                    .teacherName(teacherName)
-                    .toEmail(studentMail)
-                    .feedback(feedbackInput)
-                    .build());
-
-        }
+        Task desiredTask = taskService.getTaskById(Long.parseLong(feedbackTaskId));
+        taskService.updateTaskStage(desiredTask, "Отправлены правки");
+        notificationService.sendFeedbackToStudent(feedbackInput, desiredTask);
         return "redirect:/personalAccount";
     }
 }
 //http://localhost:8080/personalAccount?qwe=имя
+// todo: разобраться с временем истечения токена и его обновлением
