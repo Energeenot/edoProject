8b8c36418196039d17e79222914d3babc1082f6d
Energeenot <abramov.pandos@yandex.ru>
2025-03-27 20:59:40 +0300

    пользователь теперь получаю из HttpServletRequest, а не из principal

--- src/main/java/com/example/edo/controllers/HelloController.java
+++ src/main/java/com/example/edo/controllers/HelloController.java
@@ -1,12 +1,11 @@
 package com.example.edo.controllers;
 
 import com.example.edo.services.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.security.Principal;
 
 @Controller
 @RequiredArgsConstructor
@@ -15,8 +14,8 @@ public class HelloController {
     private final UserService userService;
 
     @GetMapping("/")
-    public String home(Model model, Principal principal) {
-        model.addAttribute("user", userService.getUserByPrincipal(principal));
+    public String home(Model model, HttpServletRequest request) {
+        model.addAttribute("user", userService.getUserByRequest(request));
         return "home";
     }
 }
