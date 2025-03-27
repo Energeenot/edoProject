87c3a785f4e37e564cf0e8282ce9fdecc12b74f2
Energeenot <abramov.pandos@yandex.ru>
2025-03-27 20:55:20 +0300

    добавил забытую аннотацию

--- src/main/java/com/example/edo/repositories/FilesRepository.java
+++ src/main/java/com/example/edo/repositories/FilesRepository.java
@@ -4,9 +4,11 @@ import com.example.edo.models.Files;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface FilesRepository extends JpaRepository<Files, Long> {
     @Query("SELECT f.uniqueName FROM Files f WHERE f.uniqueGroupCode = :uniqueGroupCode")
     List<String> findAllUniqueNamesByUniqueGroupCode(@Param("uniqueGroupCode") String uniqueGroupCode);
